571d3db019805d303c2de3fa64c493fe96d9ce76
ejsamrt111 <egbeejay@gmail.com>
2019-02-28 09:33:20 +0100

    Updates in the main.js with font awesome references

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faKey, faBars, faSearch, faCog, faBookReader, faUserAlt, faUserTie, faTh, faBookmark, faThLarge, faUniversity, faBookOpen, faListAlt } from "@fortawesome/free-solid-svg-icons";
+import { faBell} from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
-library.add(faKey)
+library.add(faKey, faBars, faSearch, faBookReader, faCog, faUserAlt, faUserTie, faTh, faBell, faBookmark, faThLarge, faUniversity, faListAlt, faBookOpen);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
