c90863486229d20774f981aa06588004005b5fe7
LuthfulahiO <luteeph@gmail.com>
2019-03-27 16:26:01 +0100

    removed firebase

---
+++ dist/precache-manifest.08541c1eec05c219165510470b9e6445.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8084ea1ca9e3c64636e9",
+    "url": "/js/chunk-vendors.e175b5e7.js"
+  },
+  {
+    "revision": "e1d6cf1eafb6a2c453c6",
+    "url": "/js/app.43c92c7d.js"
+  },
+  {
+    "revision": "293987058617c115194aa6f82b64d6fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1324f9190442e023eadfa25aca90a09f",
+    "url": "/img/logoW.1324f919.png"
+  },
+  {
+    "revision": "44e8e607903c6964dbe908d701b5f829",
+    "url": "/img/login.44e8e607.jpg"
+  },
+  {
+    "revision": "622ecc7e41d2d9c03de723f0d5689471",
+    "url": "/img/Library-2-700x338.622ecc7e.jpg"
+  },
+  {
+    "revision": "8084ea1ca9e3c64636e9",
+    "url": "/css/chunk-vendors.7346f4f6.css"
+  },
+  {
+    "revision": "e1d6cf1eafb6a2c453c6",
+    "url": "/css/app.e44373df.css"
+  }
+];
